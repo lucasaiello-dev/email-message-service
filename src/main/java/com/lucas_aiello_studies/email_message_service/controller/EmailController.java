15f7ac56abf9df27994952aa1d3ad132474abe6b
lucasaiello-dev <lucas.aiello@sptech.school>
2025-10-05 23:48:21 -0300

    implement log in send methods of service layer

--- src/main/java/com/lucas_aiello_studies/email_message_service/controller/EmailController.java
+++ src/main/java/com/lucas_aiello_studies/email_message_service/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.lucas_aiello_studies.email_message_service.controller;
 import com.lucas_aiello_studies.email_message_service.controller.dto.EmailRequest;
 import com.lucas_aiello_studies.email_message_service.controller.dto.EmailResponse;
 import com.lucas_aiello_studies.email_message_service.service.EmailServiceImp;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,12 @@ public class EmailController {
     private EmailServiceImp emailService;
 
     @PostMapping
-    private ResponseEntity<EmailResponse> sendEmail(@Valid @RequestBody EmailRequest emailRequest) {
-        emailService.sendSimpleEmail(emailRequest);
+    private ResponseEntity<EmailResponse> sendEmail(@Valid @RequestBody EmailRequest emailRequest) throws MessagingException {
+        if (emailRequest.getAttachment() != null && !emailRequest.getAttachment().isBlank()) {
+            emailService.sendEmailWithAttachment(emailRequest);
+        } else {
+            emailService.sendSimpleEmail(emailRequest);
+        }
         return ResponseEntity.ok(new EmailResponse(emailRequest.getTo()));
     }
 
--- src/main/java/com/lucas_aiello_studies/email_message_service/service/EmailServiceImp.java
+++ src/main/java/com/lucas_aiello_studies/email_message_service/service/EmailServiceImp.java
@@ -1,27 +1,45 @@
 package com.lucas_aiello_studies.email_message_service.service;
 
 import com.lucas_aiello_studies.email_message_service.controller.dto.EmailRequest;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class EmailServiceImp implements EmailService {
 
     @Autowired
-    private JavaMailSender emailSender;
+    private JavaMailSender sender;
 
     @Override
     public void sendSimpleEmail(EmailRequest emailRequest) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("noreply@baeldung.com");
         message.setTo(emailRequest.getTo());
         message.setSubject(emailRequest.getSubject());
         message.setText(emailRequest.getContent());
-        emailSender.send(message);
+        sender.send(message);
     }
 
     @Override
-    public void sendEmailWithAttachment(EmailRequest emailRequest) {}
+    public void sendEmailWithAttachment(EmailRequest emailRequest) throws MessagingException {
+        MimeMessage message = sender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setTo(emailRequest.getTo());
+        helper.setSubject(emailRequest.getSubject());
+        helper.setText(emailRequest.getContent());
+
+        FileSystemResource file
+                = new FileSystemResource(new File(emailRequest.getAttachment()));
+        helper.addAttachment("Request File", file);
+
+        sender.send(message);
+    }
 }
