c51fdb60219117eb154d81393c4b7f6b43e4d575
lucasaiello-dev <lucas.aiello@sptech.school>
2025-10-05 22:27:01 -0300

    inital files to build and develop project

---
+++ src/main/java/com/lucas_aiello_studies/email_message_service/EmailMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lucas_aiello_studies.email_message_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailMessageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas_aiello_studies/email_message_service/EmailMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas_aiello_studies.email_message_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailMessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
